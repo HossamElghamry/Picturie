fbecfd925c35cd2564b77d184437f23e16045320
Hossam Elghamry <ghamry_97@hotmail.com>
2019-08-18 15:16:15 +0200

    Firebase auth done

--- lib/src/ui/sign_in/widgets/sign_in_button.dart
+++ lib/src/ui/sign_in/widgets/sign_in_button.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:picturie/src/ui/tab_view/tab_view.dart';
 
 class SignInButton extends StatefulWidget {
   final TextEditingController _emailController;
@@ -26,9 +28,31 @@ class _SignInButtonState extends State<SignInButton> {
             fontSize: 20,
           ),
         ),
-        onPressed: () {
-          print(widget._emailController.text);
-          print(widget._passwordController.text);
+        onPressed: () async {
+          String _email = widget._emailController.text;
+          String _password = widget._passwordController.text;
+          // print(widget._emailController.text);
+          // print(widget._passwordController.text);
+          try {
+            FirebaseUser userId = await FirebaseAuth.instance
+                .signInWithEmailAndPassword(email: _email, password: _password);
+
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HomeTabView(),
+              ),
+            );
+          } catch (e) {
+            Scaffold.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Invalid Credentials'),
+                backgroundColor: Colors.red,
+                duration: Duration(seconds: 3),
+              ),
+            );
+            print(e.message);
+          }
         },
       ),
     );
